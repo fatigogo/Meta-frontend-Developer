b229d6dc15e2b0f602475970649e42c7f847dc60
fatigogo <fatigogo33@gmail.com>
2024-11-28 04:31:35 +0100

    Modified Styling

--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -6,6 +6,9 @@ function Nav() {
         <ul className="nav-list" style={{ display: "flex", listStyleType: "none", gap: "15px", padding: 0 }}>
           <li className="nav-item"><a href="#home">Home</a></li>
           <li className="nav-item"><a href="#menu">Menu</a></li>
+          <li><a href="#specials">Specials</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#booking">Book a Table</a></li>
           <li className="nav-item"><a href="#about">About Us</a></li>
           <li className="nav-item"><a href="#contact">Contact</a></li>
         </ul>
